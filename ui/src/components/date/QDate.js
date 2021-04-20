7960cf142013e782e522ce2f9bd9c9fe148bf706
Dmitriy Gontsov <gontsov@tochka.com>
2021-04-20 17:34:03 +0500

    fixed setView method

--- ui/src/components/date/QDate.js
+++ ui/src/components/date/QDate.js
@@ -727,9 +727,9 @@ export default defineComponent({
       setCalendarTo(today.value.year, today.value.month)
     }
 
-    function setView (view) {
-      if (viewIsValid(view) === true) {
-        view.value = view
+    function setView (targetView) {
+      if (viewIsValid(targetView) === true) {
+        view.value = targetView
       }
     }
 
